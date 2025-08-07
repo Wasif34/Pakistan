8c8e504758e2d431baf5651c1e7d2ac2d8608388
Wasif34 <wasif.shahid@acruxtek.com>
2025-08-07 14:44:06 +0500

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "PK Dashboard",
+        name: "Pakistan Economic Dashboard",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#10b981",
+      },
+    }),
+  ],
+});
